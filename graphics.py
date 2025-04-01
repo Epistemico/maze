c0c7149d840eb92619ed9ac3ff203561ed914159
Epistemico <Epistemical@outlook.com>
2025-04-02 01:24:37 +0200

    Added graphic classes Point and Line with their respective methods for GUI rendering

--- graphics.py
+++ graphics.py
@@ -7,8 +7,11 @@ class Window():
         self.__root.title("Maze Solver")
         self.__canvas = Canvas(self.__root, width=width, height=height, bg="white")
         self.__canvas.pack(fill=BOTH, expand=1)
-        self.__executing = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__executing = False
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
 
     def redraw(self):
         self.__root.update_idletasks()
@@ -22,3 +25,25 @@ class Window():
     
     def close(self):
         self.__executing = False
+
+
+class Point():
+    def __init__(self, x, y):
+        self.x_coord = x
+        self.y_coord = y
+
+
+class Line():
+    def __init__(self, point_1, point_2):
+        self.point_1 = point_1
+        self.point_2 = point_2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.point_1.x_coord, 
+            self.point_1.y_coord, 
+            self.point_2.x_coord, 
+            self.point_2.y_coord, 
+            fill=fill_color, 
+            width=2
+        )
--- main.py
+++ main.py
@@ -3,8 +3,14 @@ from graphics import *
 
 def main():
     win = Window(800, 600)
+    
+    line = Line(Point(10, 10), Point(200, 200))
+    line2 = Line(Point(10, 200), Point(200, 10))
+
+    win.draw_line(line, "black")
+    win.draw_line(line2, "red")
+    
     win.wait_for_close()
-    win.close()
 
 
 main()
